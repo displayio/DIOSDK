18af2ea5c49a7190c6c587c6ed95339e180f7a34
Roman Do <bitlejuce@gmail.com>
2025-07-02 11:26:32 +0200

    4.4.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DIOSDK",
-            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/sdk/4.4.0/DIOSDK.zip",
-            checksum: "be80df3ec1e300dc9442bfc7650e5f7f8d2ed0e8ab26b65ed788d5c8a9395352"
+            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/sdk/4.4.1/DIOSDK.zip",
+            checksum: "198bd952b1deebfd03e3a0297da936c003f2915ccec6c90c5aeb337c173852f5"
         )
     ]
 )
