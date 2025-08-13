7b3100bbfca3115eed8315992f5affe9950fd1c3
Roman Do <bitlejuce@gmail.com>
2025-08-13 14:42:09 +0200

    4.4.3

--- Package.swift
+++ Package.swift
@@ -9,14 +9,32 @@ let package = Package(
     products: [
         .library(
             name: "DIOSDK",
-            targets: ["DIOSDK"]
-        ),
+            targets: ["DIOSDKWrapper"]
+        )
     ],
     targets: [
         .binaryTarget(
             name: "DIOSDK",
-            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/sdk/4.4.2/DIOSDK.zip",
-            checksum: "7db041c53281cf12a422042ac78a052623877c427ab241a19b62634968a5c622"
+            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/sdk/4.4.3/DIOSDK.zip",
+            checksum: "72678fa201a1e3c4206d4f58eedb2b453c1b94c8cd870c75ad5fe729a1f56299"
+        ),
+        .binaryTarget(
+            name: "FBAudienceNetwork",
+            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/FBAudienceSDK/FBAudienceNetwork-6.20.1.zip",
+            checksum: "6d4827501812b47af0c3ccf74c14eb39a651fe30fb414a54a6114dda5bc793ff"
+        ),
+        .binaryTarget(
+            name: "DIOFacebookAdapter",
+            url: "https://mp-cocoapods-hosting.s3.us-west-2.amazonaws.com/fbadapter/4.4.3/DIOFacebookAdapter.zip",
+            checksum: "854490f83dd0b94679bf26509899f4a675b15cca12d2f6ff589260cac2a78ce7"
+        ),
+        .target(
+            name: "DIOSDKWrapper",
+            dependencies: [
+                "DIOSDK",
+                "FBAudienceNetwork",
+                "DIOFacebookAdapter"
+            ]
         )
     ]
 )
