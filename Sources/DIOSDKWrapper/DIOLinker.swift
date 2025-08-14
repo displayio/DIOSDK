1d25e52320ca4d3129ce756f519e242d67682868
Roman Do <bitlejuce@gmail.com>
2025-08-14 11:54:49 +0200

    DIOLinker

---
+++ Sources/DIOSDKWrapper/DIOLinker.swift
@@ -0,0 +1,13 @@
+import Foundation
+import FBAudienceNetwork
+import DIOFacebookAdapter
+
+@_used
+private let _forceFBAudienceLink: () = {
+    _ = FBAudienceNetworkAds.self
+}()
+
+@_used
+private let _forceAdapterLink: () = {
+    _ = DIOFacebookAdapterGlue.self
+}()
